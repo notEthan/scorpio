d48effa7ba305bf30f4b0d52590942472c922c8f
Ethan <ethan@unth>
2018-11-28 04:46:13 -0800

    Scorpio::OpenAPI::Operation::Configurables will contain the many configurable attributes of an Operation

--- lib/scorpio/openapi/operation.rb
+++ lib/scorpio/openapi/operation.rb
@@ -1,16 +1,25 @@
 module Scorpio
   module OpenAPI
     module Operation
+      module Configurables
+      end
+      include Configurables
     end
 
     module V3
       raise(Bug) unless const_defined?(:Operation)
       class Operation
+        module Configurables
+        end
+        include Configurables
       end
     end
     module V2
       raise(Bug) unless const_defined?(:Operation)
       class Operation
+        module Configurables
+        end
+        include Configurables
       end
     end
   end
