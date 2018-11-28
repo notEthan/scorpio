9ee7d6dfc3d091f1d7cb105207871043761f1fce
Ethan <ethan@unth>
2018-11-28 04:46:13 -0800

    Scorpio::OpenAPI::Document::Configurables will contain the many configurable attributes of a Document, with V2 and V3 modules as well

--- lib/scorpio/openapi/document.rb
+++ lib/scorpio/openapi/document.rb
@@ -1,17 +1,26 @@
 module Scorpio
   module OpenAPI
     module Document
+      module Configurables
+      end
+      include Configurables
     end
 
     module V3
       raise(Bug) unless const_defined?(:Document)
       class Document
+        module Configurables
+        end
+        include Configurables
       end
     end
 
     module V2
       raise(Bug) unless const_defined?(:Document)
       class Document
+        module Configurables
+        end
+        include Configurables
       end
     end
   end
