feacaa95ab7c2b9d38e2c126897c2e487d81bb77
Ethan <ethan@unth>
2019-01-11 19:14:37 -0800

    Scorpio::Ur overrides #class_for_schema to instantiate Scorpio::Response instead of Ur::Response

--- lib/scorpio/ur.rb
+++ lib/scorpio/ur.rb
@@ -1,5 +1,14 @@
 module Scorpio
   class Ur < ::Ur
     attr_accessor :scorpio_request
+
+    def class_for_schema(schema)
+      jsi_class_for_schema = super
+      if jsi_class_for_schema == ::Ur::Response
+        Scorpio::Response
+      else
+        jsi_class_for_schema
+      end
+    end
   end
 end
