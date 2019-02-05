eeb66c17bb8a1be782ce585f11205ed7bed52065
Ethan <ethan@unth>
2019-02-05 06:31:08 -0800

    fix error message for raise_on_http_error now that it lives in Scorpio::Ur

--- lib/scorpio/ur.rb
+++ lib/scorpio/ur.rb
@@ -21,7 +21,7 @@ module Scorpio
         HTTPError
       end
       if error_class
-        message = "Error calling operation #{scorpio_request.operation.operationId} on #{self}:\n" + response.body
+        message = "Error calling operation #{scorpio_request.operation.operationId}:\n" + response.body
         raise(error_class.new(message).tap do |e|
           e.ur = self
           e.response_object = response.body_object
