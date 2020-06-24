b2dfb728db85909bc943912a79d073e7584d8709
Ethan <ethan@unth.net>
2020-06-24 07:21:58 -0700

    implement Scorpio::OpenAPI::Reference#deref
    JSI::Base#deref removed in https://github.com/notEthan/jsi/pull/124/commits/ff220eb9b24659c62e83bd1e17cddbdf0caca42b

--- lib/scorpio/openapi/reference.rb
+++ lib/scorpio/openapi/reference.rb
@@ -14,6 +14,28 @@ module Scorpio
         end
         return super
       end
+
+      def deref
+        return unless respond_to?(:to_hash) && self['$ref'].respond_to?(:to_str)
+
+        ref_uri = Addressable::URI.parse(self['$ref'])
+        ref_uri_nofrag = ref_uri.merge(fragment: nil)
+
+        if !ref_uri_nofrag.empty? || ref_uri.fragment.nil?
+          raise(NotImplementedError,
+            "Scorpio currently only supports fragment URIs as OpenAPI references. cannot find reference by uri: #{self['$ref']}"
+          )
+        end
+
+        ptr = JSI::JSON::Pointer.from_fragment(ref_uri.fragment)
+        deref_jsi = ptr.evaluate(jsi_root_node)
+
+        # TODO type check deref_jsi
+
+        yield deref_jsi
+
+        nil
+      end
     end
   end
 end
