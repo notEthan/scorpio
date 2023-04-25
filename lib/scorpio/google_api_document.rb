be80f7055e0453c9cdb0f2bc75b3a7cc8f0fe7de
Ethan <ethan@unth.net>
2023-04-25 12:34:34 -0700

    freeze google discovery rest description

--- lib/scorpio/google_api_document.rb
+++ lib/scorpio/google_api_document.rb
@@ -2,7 +2,7 @@
 
 module Scorpio
   module Google
-    discovery_rest_description_doc = ::JSON.parse(Scorpio.root.join('documents/www.googleapis.com/discovery/v1/apis/discovery/v1/rest').read)
+    discovery_rest_description_doc = JSON.parse(Scorpio.root.join('documents/www.googleapis.com/discovery/v1/apis/discovery/v1/rest').read, freeze: true)
     discovery_rest_description = JSI::MetaSchemaNode.new(
       discovery_rest_description_doc,
       metaschema_root_ptr: JSI::Ptr['schemas']['JsonSchema'],
