8cb2e8f149182653097c74f7dd2a9a02154c0be5
Ethan <ethan@unth.net>
2024-11-19 18:02:36 -0800

    mv Scorpio::OpenAPI::V2, Scorpio::OpenAPI::V3 to their own files

---
+++ lib/scorpio/openapi/v2.rb
@@ -0,0 +1,107 @@
+# frozen_string_literal: true
+
+module Scorpio
+  module OpenAPI
+    module V2
+      Document = JSI.new_schema_module(JSON.parse(Scorpio.root.join(
+        'documents/swagger.io/v2/schema.json'
+      ).read))
+
+      # the schema represented by Scorpio::OpenAPI::V2::Schema will describe schemas itself.
+      # JSI::Schema#describes_schema! enables this to implement the functionality of schemas.
+      describe_schema = [
+        Document.schema.definitions['schema'],
+        # comments on V3's definitions['Schema'].properties['additionalProperties'] apply here too
+        Document.schema.definitions['schema'].properties['additionalProperties'],
+      ]
+      describe_schema.each { |s| s.describes_schema!([JSI::Schema::Draft04]) }
+
+      # naming these is not strictly necessary, but is nice to have.
+      # generated: `puts Scorpio::OpenAPI::V2::Document.schema.definitions.keys.map { |k| "#{k[0].upcase}#{k[1..-1]} = Document.definitions['#{k}']" }`
+
+
+      Info            = Document.definitions['info']
+      Contact          = Document.definitions['contact']
+      License           = Document.definitions['license']
+      Paths              = Document.definitions['paths']
+      Definitions         = Document.definitions['definitions']
+      ParameterDefinitions = Document.definitions['parameterDefinitions']
+      ResponseDefinitions = Document.definitions['responseDefinitions']
+      ExternalDocs       = Document.definitions['externalDocs']
+      Examples          = Document.definitions['examples']
+      MimeType         = Document.definitions['mimeType']
+      Operation       = Document.definitions['operation']
+      PathItem         = Document.definitions['pathItem']
+      Responses         = Document.definitions['responses']
+      ResponseValue      = Document.definitions['responseValue']
+      Response            = Document.definitions['response']
+      Headers              = Document.definitions['headers']
+      Header                = Document.definitions['header']
+      VendorExtension        = Document.definitions['vendorExtension']
+      BodyParameter           = Document.definitions['bodyParameter']
+      HeaderParameterSubSchema = Document.definitions['headerParameterSubSchema']
+      QueryParameterSubSchema   = Document.definitions['queryParameterSubSchema']
+      FormDataParameterSubSchema = Document.definitions['formDataParameterSubSchema']
+      PathParameterSubSchema    = Document.definitions['pathParameterSubSchema']
+      NonBodyParameter         = Document.definitions['nonBodyParameter']
+      Parameter               = Document.definitions['parameter']
+      Schema                 = Document.definitions['schema']
+      FileSchema            = Document.definitions['fileSchema']
+      PrimitivesItems       = Document.definitions['primitivesItems']
+      Security               = Document.definitions['security']
+      SecurityRequirement     = Document.definitions['securityRequirement']
+      Xml                      = Document.definitions['xml']
+      Tag                       = Document.definitions['tag']
+      SecurityDefinitions        = Document.definitions['securityDefinitions']
+      BasicAuthenticationSecurity = Document.definitions['basicAuthenticationSecurity']
+      ApiKeySecurity             = Document.definitions['apiKeySecurity']
+      Oauth2ImplicitSecurity    = Document.definitions['oauth2ImplicitSecurity']
+      Oauth2PasswordSecurity   = Document.definitions['oauth2PasswordSecurity']
+      Oauth2ApplicationSecurity = Document.definitions['oauth2ApplicationSecurity']
+      Oauth2AccessCodeSecurity = Document.definitions['oauth2AccessCodeSecurity']
+      Oauth2Scopes            = Document.definitions['oauth2Scopes']
+      MediaTypeList          = Document.definitions['mediaTypeList']
+      ParametersList         = Document.definitions['parametersList']
+      SchemesList             = Document.definitions['schemesList']
+      CollectionFormat         = Document.definitions['collectionFormat']
+      CollectionFormatWithMulti = Document.definitions['collectionFormatWithMulti']
+      Title                    = Document.definitions['title']
+      Description             = Document.definitions['description']
+      Default                = Document.definitions['default']
+      MultipleOf            = Document.definitions['multipleOf']
+      Maximum              = Document.definitions['maximum']
+      ExclusiveMaximum    = Document.definitions['exclusiveMaximum']
+      Minimum            = Document.definitions['minimum']
+      ExclusiveMinimum  = Document.definitions['exclusiveMinimum']
+      MaxLength        = Document.definitions['maxLength']
+      MinLength       = Document.definitions['minLength']
+      Pattern        = Document.definitions['pattern']
+      MaxItems      = Document.definitions['maxItems']
+      MinItems     = Document.definitions['minItems']
+      UniqueItems = Document.definitions['uniqueItems']
+      Enum         = Document.definitions['enum']
+      JsonReference = Document.definitions['jsonReference']
+
+      module Operation
+        include(OpenAPI::Operation::V2Methods)
+      end
+
+      # A document that defines or describes an API conforming to the OpenAPI Specification v2 (aka Swagger).
+      #
+      # The root document is known as the Swagger Object.
+      module Document
+        include(OpenAPI::Document::V2Methods)
+      end
+
+      module JsonReference
+        include(OpenAPI::Reference)
+      end
+
+      module Tag
+        include(OpenAPI::Tag)
+      end
+
+      raise(Bug) unless Schema < JSI::Schema
+    end
+  end
+end
---
+++ lib/scorpio/openapi/v3_0.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+module Scorpio
+  module OpenAPI
+    module V3
+      Document = JSI::JSONSchemaDraft04.new_schema_module(YAML.safe_load(Scorpio.root.join(
+        'documents/spec.openapis.org/oas/3.0/schema.yaml'
+      ).read))
+
+      # the schema represented by Scorpio::OpenAPI::V3::Schema will describe schemas itself.
+      # JSI::Schema#describes_schema! enables this to implement the functionality of schemas.
+      describe_schema = [
+        Document.schema.definitions['Schema'],
+        Document.schema.definitions['SchemaReference'],
+        # instead of the Schema definition allowing boolean, properties['additionalProperties']
+        # is a oneOf which allows a Schema, SchemaReference, or boolean.
+        # instances of the former two already include the schema implementation (per the previous
+        # describes_schema entries), but the boolean does not.
+        # including in properties['additionalProperties'] applies to any additionalProperties.
+        # (including in properties['additionalProperties'].anyOf[2] would extend booleans too, without
+        # the redundant inclusion that results for Schema and SchemaRef, but redundant inclusion is not
+        # a problem, and this way also applies when none of the anyOf match due to schema errors.)
+        Document.schema.definitions['Schema'].properties['additionalProperties'],
+      ]
+      describe_schema.each { |s| s.describes_schema!([JSI::Schema::Draft04]) }
+
+      # naming these is not strictly necessary, but is nice to have.
+      # generated: `puts Scorpio::OpenAPI::V3::Document.schema.definitions.keys.map { |k| "#{k[0].upcase}#{k[1..-1]} = Document.definitions['#{k}']" }`
+
+
+      Reference      = Document.definitions['Reference']
+      SchemaReference = Document.definitions['SchemaReference']
+      Info           = Document.definitions['Info']
+      Contact       = Document.definitions['Contact']
+      License      = Document.definitions['License']
+      Server        = Document.definitions['Server']
+      ServerVariable = Document.definitions['ServerVariable']
+      Components    = Document.definitions['Components']
+      Schema       = Document.definitions['Schema']
+      Discriminator = Document.definitions['Discriminator']
+      XML          = Document.definitions['XML']
+      Response    = Document.definitions['Response']
+      MediaType    = Document.definitions['MediaType']
+      Example       = Document.definitions['Example']
+      Header         = Document.definitions['Header']
+      Paths           = Document.definitions['Paths']
+      PathItem         = Document.definitions['PathItem']
+      Operation         = Document.definitions['Operation']
+      Responses          = Document.definitions['Responses']
+      SecurityRequirement = Document.definitions['SecurityRequirement']
+      Tag                  = Document.definitions['Tag']
+      ExternalDocumentation = Document.definitions['ExternalDocumentation']
+      ExampleXORExamples   = Document.definitions['ExampleXORExamples']
+      SchemaXORContent    = Document.definitions['SchemaXORContent']
+      Parameter          = Document.definitions['Parameter']
+      PathParameter      = Document.definitions['PathParameter']
+      QueryParameter      = Document.definitions['QueryParameter']
+      HeaderParameter      = Document.definitions['HeaderParameter']
+      CookieParameter       = Document.definitions['CookieParameter']
+      RequestBody            = Document.definitions['RequestBody']
+      SecurityScheme          = Document.definitions['SecurityScheme']
+      APIKeySecurityScheme     = Document.definitions['APIKeySecurityScheme']
+      HTTPSecurityScheme        = Document.definitions['HTTPSecurityScheme']
+      OAuth2SecurityScheme       = Document.definitions['OAuth2SecurityScheme']
+      OpenIdConnectSecurityScheme = Document.definitions['OpenIdConnectSecurityScheme']
+      OAuthFlows                 = Document.definitions['OAuthFlows']
+      ImplicitOAuthFlow         = Document.definitions['ImplicitOAuthFlow']
+      PasswordOAuthFlow        = Document.definitions['PasswordOAuthFlow']
+      ClientCredentialsFlow     = Document.definitions['ClientCredentialsFlow']
+      AuthorizationCodeOAuthFlow = Document.definitions['AuthorizationCodeOAuthFlow']
+      Link                      = Document.definitions['Link']
+      Callback                 = Document.definitions['Callback']
+      Encoding                = Document.definitions['Encoding']
+
+      # Describes a single API operation on a path.
+      #
+      # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#operationObject
+      module Operation
+        include(OpenAPI::Operation::V3Methods)
+      end
+
+      # A document that defines or describes an API conforming to the OpenAPI Specification v3.
+      #
+      # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#oasObject
+      module Document
+        include(OpenAPI::Document::V3Methods)
+      end
+
+      module Reference
+        include(OpenAPI::Reference)
+      end
+
+      module Tag
+        include(OpenAPI::Tag)
+      end
+
+      # An object representing a Server.
+      #
+      # https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#serverObject
+      module Server
+        include(OpenAPI::Server)
+      end
+
+      raise(Bug) unless Schema < JSI::Schema
+      raise(Bug) unless SchemaReference < JSI::Schema
+    end
+  end
+end
