5015887fa2c11bf71636a715636a7209ec438c0b
Ethan <ethan@unth.net>
2024-12-03 13:11:40 -0800

    test blog article read error fix path

--- test/scorpio_test.rb
+++ test/scorpio_test.rb
@@ -51,12 +51,13 @@ describe 'blog' do
     e = assert_raises(ArgumentError) do
       Article.read({})
     end
-    assert_equal('path /articles/{id} for operation articles.read requires path_params which were missing: ["id"]',
+    path = BlogModel.openapi_document.operations['articles.read'].path_template_str
+    assert_equal(%Q(path #{path} for operation articles.read requires path_params which were missing: ["id"]),
       e.message)
     e = assert_raises(ArgumentError) do
       Article.read({id: ''})
     end
-    assert_equal('path /articles/{id} for operation articles.read requires path_params which were empty: ["id"]',
+    assert_equal(%Q(path #{path} for operation articles.read requires path_params which were empty: ["id"]),
       e.message)
   end
   it 'tries to read a nonexistent article' do
