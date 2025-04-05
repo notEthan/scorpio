29fe1ad20413ace0ec8e46da59e797221a1a55e4
Ethan <ethan@unth.net>
2025-04-05 12:38:28 -0700

    OpenAPI::Reference /#deref/#resolve/

--- lib/scorpio/openapi/reference.rb
+++ lib/scorpio/openapi/reference.rb
@@ -3,13 +3,13 @@
 module Scorpio
   module OpenAPI
     module Reference
-      # overrides JSI::Base#[] to implicitly dereference this Reference, except when
+      # overrides JSI::Base#[] to implicitly resolve this Reference, except when
       # the given token is present in this Reference's instance (this should usually
       # only apply to the token '$ref')
       def [](token, **kw)
         if respond_to?(:to_hash) && !key?(token)
-          deref do |deref_jsi|
-            return(deref_jsi[token, **kw])
+          resolve do |resolved|
+            return(resolved[token, **kw])
           end
         end
         return super
@@ -18,7 +18,7 @@ module Scorpio
       # yields or returns the target of this reference
       # @yield [JSI::Base] if a block is given
       # @return [JSI::Base]
-      def deref
+      def resolve
         return unless respond_to?(:to_hash) && key?('$ref') && jsi_node_content['$ref'].respond_to?(:to_str)
 
         ref = @memos.fetch(:oa_ref) { @memos[:oa_ref] = JSI::Ref.new(jsi_node_content['$ref'], referrer: self) }
