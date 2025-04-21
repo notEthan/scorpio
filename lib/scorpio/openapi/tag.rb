528e84c009bf4b5ecbd2f9117341b08e02dcd2d6
Ethan <ethan@unth.net>
2025-04-21 00:42:28 -0700

    OpenAPI::Tag + OpenAPI::Document::Descendent

--- lib/scorpio/openapi/tag.rb
+++ lib/scorpio/openapi/tag.rb
@@ -1,6 +1,8 @@
 module Scorpio
   module OpenAPI
     module Tag
+      include(Document::Descendent)
+
       # operations in the openapi document which have a tag with this tag's name
       # @return [Enumerable<Scorpio::OpenAPI::Operation>]
       def operations
@@ -9,13 +11,9 @@ module Scorpio
       end
 
       def each_operation(&block)
-        unless jsi_root_node.is_a?(OpenAPI::Document)
-          raise("Tag#each_operation cannot be used on a Tag that is not inside an OpenAPI document")
-        end
-
         return(to_enum(__method__)) unless block
 
-        jsi_root_node.each_operation do |op|
+        openapi_document.each_operation do |op|
           yield(op) if op.tags.respond_to?(:to_ary) && op.tags.include?(name)
         end
       end
