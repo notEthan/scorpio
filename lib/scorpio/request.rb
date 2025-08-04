0599321c77c26259ca85633fb919968d35fc82cc
Ethan <ethan@unth.net>
2025-08-04 13:30:09 -0700

    fix error message using removed Request::SUPPORTED_REQUEST_MEDIA_TYPES
    missed in 348a2ddb23b8150e647bd1b42aafc98bb2c161e1

--- lib/scorpio/request.rb
+++ lib/scorpio/request.rb
@@ -83,7 +83,7 @@ module Scorpio
             if body_object.respond_to?(:to_str)
               body_object
             else
-              raise(NotImplementedError, "Scorpio does not know how to generate the request body with content_type = #{content_type.respond_to?(:to_str) ? content_type : content_type.inspect} for operation: #{operation.human_id}. Scorpio supports media types: #{SUPPORTED_REQUEST_MEDIA_TYPES.join(', ')}. body_object was: #{body_object.pretty_inspect.chomp}")
+              raise(NotImplementedError, "Scorpio does not know how to generate the request body with content_type = #{content_type.respond_to?(:to_str) ? content_type : content_type.inspect} for operation: #{operation.human_id}. Scorpio supports media types: #{SUPPORTED_MEDIA_TYPES.join(', ')}. body_object was: #{body_object.pretty_inspect.chomp}")
             end
           end
         else
