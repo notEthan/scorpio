8f0e0f95e5e86f9ebd46775abf7098fd90264ae6
Ethan <ethan@unth.net>
2026-02-22 04:48:49 -0800

    ResourceBase::Container .inspect show schemas

--- lib/scorpio/resource_base.rb
+++ lib/scorpio/resource_base.rb
@@ -513,6 +513,8 @@ module Scorpio
             include(mod)
           end
 
+          define_singleton_method(:container_schemas) { schemas }
+
           schemas.each do |schema|
             include(JSI::SchemaClasses.schema_property_reader_module(schema, conflicting_modules: modules + [Container]))
             include(JSI::SchemaClasses.schema_property_writer_module(schema, conflicting_modules: modules + [Container]))
@@ -539,7 +541,13 @@ module Scorpio
 
         # @return [String]
         def inspect
-          -"(#{ResourceBase::Container.name} class)"
+          return super unless respond_to?(:container_schemas)
+          schema_names = container_schemas.map do |schema|
+            mod_name = schema.jsi_schema_module_name_from_ancestor
+            next -"#{mod_name} <#{schema.jsi_resource_uri}>" if mod_name && schema.jsi_resource_uri
+            mod_name || -"<#{schema.schema_uri || schema.jsi_ptr.uri}>"
+          end
+          -"(#{[superclass, *schema_names].join(' + ')})"
         end
 
         # @return [String]
